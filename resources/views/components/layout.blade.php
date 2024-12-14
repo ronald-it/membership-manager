99a1d57fec9bb1f186788e557c7f654ce05ede19
ronald-it <ronald.cirgofer@hotmail.com>
2024-12-14 18:57:18 +0700

    Nav section design for laptop and up implemented. Al that is left for the design for laptop and up on the homepage is the content section.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,13 +20,31 @@
             <span class="text-base sm:text-lg uppercase">ledenadministratie</span>
         </header>
 
-        <nav class="bg-theme-lavender border-y lg:border-y-0 lg:border-t lg:border-r border-gray-300 flex lg:flex-col flex-wrap justify-between lg:justify-start text-xs sm:text-sm gap-x-2 p-4 sm:p-8 [&>*]:p-2 [&>*]:rounded-3xl lg:grid-in-[nav]">
-            <a href="/" class="bg-theme-purple text-white">Families</a>
-            <a href="/member-type">Soort lid</a>
-            <a href="/contributions">Contributies</a>
-            <a href="/" class="text-red-700">Log uit</a>
-            <a href="/login" class="">Log in</a>
-            <a href="/registration" class="">Registreer</a>
+        <nav class="bg-theme-lavender lg:[&>*]:flex lg:[&>*]:gap-x-2 border-y lg:border-y-0 lg:border-t lg:border-r border-gray-300 flex lg:flex-col flex-wrap justify-between lg:justify-start text-xs sm:text-sm gap-x-2 p-4 sm:p-8 [&>*]:p-2 lg:[&>*]:p-4 [&>*]:rounded-3xl lg:grid-in-[nav]">
+            <a href="/" class="bg-theme-purple text-white">
+                <img class="hidden lg:block" src="images/overview icon white.png"/>
+                <span class="block first-letter:uppercase">families</span>
+            </a>
+            <a href="/member-type">
+                <img class="hidden lg:block" src="images/member type icon black.png"/>
+                <span class="block first-letter:uppercase">soort lid</span>
+            </a>
+            <a href="/contributions">
+                <img class="hidden lg:block" src="images/contributions icon black.png"/>
+                <span class="block first-letter:uppercase">contributies</span>
+            </a>
+            <a href="/" class="text-[#CD0000]">
+                <img class="hidden lg:block" src="images/logout icon.png"/>
+                <span class="block first-letter:uppercase">log uit</span>
+            </a>
+            <a href="/login" class="">
+                <img class="hidden lg:block" src="images/login icon.png"/>
+                <span class="block first-letter:uppercase">log in</span>
+            </a>
+            <a href="/registration" class="">
+                <img class="hidden lg:block" src="images/member type icon black.png"/>
+                <span class="block first-letter:uppercase">registreer</span>
+            </a>
         </nav>
 
         <main class="grow lg:grid-in-[content] lg:border-t border-gray-300">
@@ -39,7 +57,7 @@
             </a>
             <div class="flex flex-col sm:flex-row sm:gap-x-1 sm:items-center sm:order-first">
                     <div>© 2024
-                        <span class="text-theme-purple capitalize">membership manager</span>
+                        <span class="text-theme-purple capitalize">Ledenadministratie</span>
                     </div>
                     <span class="hidden sm:block">|</span>
                 <span class="capitalize">all rights reserved.</span>
