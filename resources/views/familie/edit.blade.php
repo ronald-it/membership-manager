84d04f6f4da36121b47df031d3f16a5c2dc8f52f
ronald-it <ronald.cirgofer@hotmail.com>
2024-12-20 20:02:46 +0700

    Mobile design changed, as the family member needs to be added on the family page as well, furthermore the edit button for the family member has been changed to a delete button as family members can not be edited.

--- resources/views/familie/edit.blade.php
+++ resources/views/familie/edit.blade.php
@@ -5,7 +5,7 @@
   <div class="flex flex-col gap-y-6 p-4">
     <section class="flex justify-center">
       <div class="flex flex-col items-center gap-y-3 text-xs w-full max-w-md">
-        <span class="uppercase font-medium text-base self-start">familie</span>
+        <span class="uppercase font-medium text-base self-start">familie bewerken</span>
         <div class="w-full">
           <div class="grow flex items-center">
             <label class="font-medium w-10 capitalize">
@@ -40,14 +40,7 @@
 
     <section class="flex justify-center">
       <div class="flex flex-col gap-y-3 w-full max-w-md">
-        <div class="flex justify-between items-center">
-          <span class="uppercase font-medium">familieleden</span>
-          <button href='/familielid' class="bg-theme-purple flex items-center gap-x-2 p-3 text-white rounded-lg w-fit text-xs uppercase">
-            <img src="/images/add icon.png"/>
-            <span>voeg nieuwe lid toe</span>
-          </button>
-        </div>
-    
+          <span class="uppercase font-medium">familieleden bewerken</span>
         <table class="w-full text-xs">
           <thead>
             <tr class="capitalize border-b border-gray-300">
@@ -61,15 +54,15 @@
           </thead>
           <tbody>
             @foreach ($familieleden as $familielid)
-            <tr class="border-b border-gray-300 last:border-b-0">
+            <tr class="border-b border-gray-300">
               <td class="hidden">{{$familielid->id}}</td>
               <td class="py-4">{{$familielid->naam}}</td>
               <td class="hidden">{{$familielid->geboortedatum}}</td>
               <td class="hidden">{{$familielid->lidsoort}}</td>
               <td class="text-center">€{{$familielid->contributie}}</td>
-              <td>
-                <a class="flex justify-end">
-                  <img src="/images/edit icon.png"/>
+              <td class="flex justify-end py-4">
+                <a class="bg-theme-red p-1 rounded">
+                  <img src="/images/delete icon.png"/>
                   <span class="hidden">Bewerken</span>
                 </a>
               </td>
@@ -77,6 +70,35 @@
             @endforeach
           </tbody>
         </table>
+      </div>
+    </section>
+
+    <section class="flex justify-center">
+      <div class="flex flex-col items-center gap-y-3 text-xs w-full max-w-md">
+        <span class="uppercase font-medium text-base self-start">familielid toevoegen</span>
+        <div class="w-full">
+          <div class="grow flex items-center">
+            <label class="font-medium w-24 capitalize">
+              naam
+            </label>
+            <input type="text" class="border border-theme-purple rounded grow p-2"/>
+          </div>
+        </div>
+  
+        <div class="w-full">
+          <div class="grow flex items-center">
+            <label class="font-medium w-24 capitalize">
+              geboortedatum
+            </label>
+            <input type="date" class="border border-theme-purple rounded grow p-2"/>
+          </div>
+        </div>
+  
+        <button class="bg-theme-purple flex justify-center items-center gap-x-2 text-white rounded-lg p-3 uppercase w-full">
+          <img src="/images/add icon.png"/>
+          <span>voeg nieuwe lid toe</span>
+        </button>
+  
       </div>
     </section>
 
