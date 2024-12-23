2f341a307878c3e24024b212ef92313388021d2c
ronald-it <ronald.cirgofer@hotmail.com>
2024-12-23 18:51:33 +0700

    Mobile design for member type edit page has been implemented. Next up is making it responsive for tablet and laptop and up.

--- resources/views/lidsoort/edit.blade.php
+++ resources/views/lidsoort/edit.blade.php
@@ -2,9 +2,20 @@
   <x-slot:title>
     Soort lid bewerken
   </x-slot>
-  <div class="flex flex-col">
-    <span>soort lid bewerken</span>
-    <span>{{$lidsoort->id}}</span>
-    <span>{{$lidsoort->omschrijving}}</span>
+  <div class="p-4 flex flex-col items-center grow">
+    <form method="POST" action="/lidsoort/{{$lidsoort->id}}" class="flex flex-col gap-y-4 text-xs w-full max-w-md">
+      @csrf
+      @method('PUT')
+      <span class="uppercase font-medium text-sm">soort lid bewerken</span>
+      <div class="flex items-center gap-x-2">
+        <label class="font-medium capitalize w-24" for="id">id</label>
+        <input type="text" class="border border-theme-purple rounded px-2 h-10 grow" id="id" name="id" value="{{$lidsoort->id}}" readonly required />
+      </div>
+      <div class="flex items-center gap-x-2">
+        <label class="font-medium capitalize w-24" for="omschrijving">omschrijving</label>
+        <input type="text" class="border border-theme-purple rounded px-2 h-10 grow" id="omschrijving" name="omschrijving" value="{{$lidsoort->omschrijving}}" required />
+      </div>
+      <button class="bg-theme-purple flex justify-center items-center text-white rounded-lg h-10 uppercase w-full">bevestigen</button>
+    </form>
   </div>
 </x-layout>
