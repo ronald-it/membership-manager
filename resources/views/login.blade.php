923198958bd0ac0ebe9c9cf6e3985ea6003cc59f
ronald-it <ronald.cirgofer@hotmail.com>
2024-12-25 18:05:52 +0700

    Design for all screen sizes for login page and registration page has been implemented. Now functionality has to be implemented for those pages.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,5 +2,19 @@
   <x-slot:title>
     Login
   </x-slot>
-  <span>Login</span>
+  <div class="bg-theme-light-purple grow flex items-center justify-center p-4">
+    <form class="bg-white shadow-md rounded-lg flex flex-col gap-y-4 p-4 sm:p-8 w-full max-w-xs sm:max-w-md" action="/login" method="POST">
+      @csrf
+      <span class="text-center uppercase font-medium text-xl">inloggen</span>
+        <div class="flex flex-col gap-y-1">
+          <label class="font-medium capitalize" for="gebruikersnaam">gebruikersnaam</label>
+          <input type="number" class="border border-theme-purple rounded px-2 h-10 " id="gebruikersnaam" name="gebruikersnaam" required />
+        </div>
+        <div class="flex flex-col gap-y-1">
+          <label class="font-medium capitalize" for="wachtwoord">wachtwoord</label>
+          <input type="number" class="border border-theme-purple rounded px-2 h-10 grow" id="wachtwoord" name="wachtwoord" required />
+        </div>
+      <button class="bg-theme-purple flex justify-center items-center text-white rounded-lg h-10 uppercase" type="submit">log in</button>
+    </form>
+  </div>
 </x-layout>
--- resources/views/registratie.blade.php
+++ resources/views/registratie.blade.php
@@ -2,5 +2,23 @@
   <x-slot:title>
     Registratie
   </x-slot>
-  <span>Registratie</span>
+  <div class="bg-theme-light-purple grow flex items-center justify-center p-4">
+    <form class="bg-white shadow-md rounded-lg flex flex-col gap-y-4 p-4 sm:p-8 w-full max-w-xs sm:max-w-md" action="/login" method="POST">
+      @csrf
+      <span class="text-center uppercase font-medium text-xl">registratie</span>
+      <div class="flex flex-col gap-y-1">
+        <label class="font-medium first-letter:uppercase" for="email">e-mailadres</label>
+        <input type="number" class="border border-theme-purple rounded px-2 h-10 " id="email" name="email" required />
+      </div>
+        <div class="flex flex-col gap-y-1">
+          <label class="font-medium first-letter:uppercase" for="gebruikersnaam">gebruikersnaam</label>
+          <input type="number" class="border border-theme-purple rounded px-2 h-10 " id="gebruikersnaam" name="gebruikersnaam" required />
+        </div>
+        <div class="flex flex-col gap-y-1">
+          <label class="font-medium first-letter:uppercase" for="wachtwoord">wachtwoord</label>
+          <input type="number" class="border border-theme-purple rounded px-2 h-10 grow" id="wachtwoord" name="wachtwoord" required />
+        </div>
+      <button class="bg-theme-purple flex justify-center items-center text-white rounded-lg h-10 uppercase" type="submit">registreer</button>
+    </form>
+  </div>
 </x-layout>
