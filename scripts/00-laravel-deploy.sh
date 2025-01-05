8c156c6954f8fcd0392c3edc5de77b67217280ac
ronald-it <ronald.cirgofer@hotmail.com>
2025-01-06 02:06:44 +0700

    Change to migrate and seed command.

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,8 +9,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
-
-echo "Seeding..."
-php artisan db:seed
+echo "Running migrations and seeding..."
+php artisan migrate:fresh --seed --force
