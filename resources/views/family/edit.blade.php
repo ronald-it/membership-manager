a26239245df54cbf394da0ddc99f207a96c5457e
ronald-it <ronald.cirgofer@hotmail.com>
2025-01-07 02:27:21 +0700

    Changed text sizes on family edit page to prevent zoom in on input focus on iPhone.

--- resources/views/family/edit.blade.php
+++ resources/views/family/edit.blade.php
@@ -6,18 +6,18 @@
     <form class="flex justify-center" action="/family/{{$family->id}}" method="POST">
       @csrf
       @method('PUT')
-      <div class="flex flex-col sm:flex-row sm:flex-wrap items-center gap-y-3 sm:gap-x-3 text-xs sm:text-sm w-full max-w-md sm:max-w-3xl lg:max-w-7xl">
-        <span class="uppercase font-medium text-sm sm:text-base self-start sm:w-full">family</span>
+      <div class="flex flex-col sm:flex-row sm:flex-wrap items-center gap-y-3 sm:gap-x-3 w-full max-w-md sm:max-w-3xl lg:max-w-7xl">
+        <span class="uppercase font-medium self-start sm:w-full">family</span>
         <div class="w-full sm:w-auto">
-          <div class="grow flex items-center">
+          <div class="grow flex flex-col sm:flex-row sm:items-center">
             <label class="font-medium w-12 sm:w-fit sm:mr-3 capitalize" for="id">
               id
             </label>
-            <input type="number" value="{{$family->id}}" id="id" name="id" readonly="true" class="border border-theme-purple rounded grow p-2 sm:w-10 sm:py-0 sm:h-8"/>
+            <input type="number" value="{{$family->id}}" id="id" name="id" readonly="true" class="border border-theme-purple rounded grow p-2 sm:w-16 sm:py-0 sm:h-8"/>
           </div>
         </div>
         <div class="grow w-full sm:w-auto">
-          <div class="grow flex items-center">
+          <div class="grow flex flex-col sm:flex-row sm:items-center">
             <label class="font-medium w-12 sm:w-fit sm:mr-3 capitalize" for="name">
               name
             </label>
@@ -25,11 +25,11 @@
           </div>
         </div>
         <div class="grow w-full sm:w-auto">
-          <div class="grow flex items-center">
+          <div class="grow flex flex-col sm:flex-row sm:items-center">
             <label class="font-medium w-12 sm:w-fit sm:mr-3 capitalize" for="address">
               address
             </label>
-            <input type="text" value="{{$family->address}}" id="address" name="address" class="border border-theme-purple rounded grow p-2 sm:py-0 sm:h-8"/>
+            <input type="text" value="{{$family->address}}" id="address" name="address" class="border border-theme-purple rounded grow p-2 sm:w-20 sm:py-0 sm:h-8"/>
           </div>
         </div>
         <button type="submit" class="bg-theme-purple flex justify-center items-center text-white rounded-lg p-3 sm:py-0 sm:h-8 uppercase w-full sm:w-fit">confirm</button>
@@ -39,17 +39,17 @@
     <section class="flex justify-center">
       <div class="flex flex-col gap-y-3 w-full max-w-md sm:max-w-3xl lg:max-w-7xl">
         <div class="flex justify-between items-center">
-          <span class="uppercase font-medium text-sm sm:text-base">family members</span>
+          <span class="uppercase font-medium">family members</span>
           <form action="/family/{{$family->id}}" method="POST">
             @csrf
             @method('DELETE')
-            <button type="submit" class="bg-theme-red text-white p-2 text-xs sm:text-sm rounded uppercase flex items-center gap-x-2 text-nowrap">
+            <button type="submit" class="bg-theme-red text-white p-2 rounded uppercase flex items-center gap-x-2 text-nowrap">
               <img src='/images/delete icon.png'/>
               <span>delete family</span>
             </button>
           </form>
         </div>
-        <table class="w-full text-xs sm:text-sm">
+        <table class="w-full">
           <thead>
             <tr class="capitalize border-b border-gray-300">
               <th class="hidden sm:table-cell text-left">id</th>
@@ -87,22 +87,22 @@
 
     <form class="flex justify-center" action="/family/{{$family->id}}/family-member" method="POST">
       @csrf
-      <div class="flex flex-col sm:flex-row sm:flex-wrap items-center gap-y-3 sm:gap-x-3 text-xs sm:text-sm w-full max-w-md sm:max-w-3xl lg:max-w-7xl">
-        <span class="uppercase font-medium text-sm sm:text-base self-start sm:w-full">add new family member</span>
+      <div class="flex flex-col sm:flex-row sm:flex-wrap items-center gap-y-3 sm:gap-x-3 w-full max-w-md sm:max-w-3xl lg:max-w-7xl">
+        <span class="uppercase font-medium self-start sm:w-full">add new family member</span>
         <div class="w-full sm:w-auto grow">
-          <div class="grow flex items-center">
+          <div class="grow flex flex-col sm:flex-row sm:items-center">
             <label class="font-medium w-24 sm:w-fit sm:mr-3 capitalize" for="name">
               name
             </label>
-            <input type="text" class="border border-theme-purple rounded grow p-2 sm:w-20 sm:py-0 sm:h-8" id="name" name="name"/>
+            <input type="text" class="border border-theme-purple rounded grow p-2 sm:w-16 sm:py-0 sm:h-8" id="name" name="name"/>
           </div>
         </div>
         <div class="w-full sm:w-auto">
-          <div class="grow flex items-center">
+          <div class="grow flex flex-col sm:flex-row sm:items-center">
             <label class="font-medium w-24 sm:w-fit sm:mr-3 first-letter:uppercase" for="date_of_birth">
               date of birth
             </label>
-            <input type="date" class="border border-theme-purple rounded grow p-2 sm:py-0 sm:h-8 sm:w-28" id="date_of_birth" name="date_of_birth"/>
+            <input type="date" class="border border-theme-purple rounded grow p-2 sm:py-0 sm:h-8 sm:w-32" id="date_of_birth" name="date_of_birth"/>
           </div>
         </div>
         <button class="bg-theme-purple flex justify-center items-center gap-x-2 text-white rounded-lg p-3 sm:py-0 sm:h-8 uppercase w-full sm:w-fit" type="submit">
