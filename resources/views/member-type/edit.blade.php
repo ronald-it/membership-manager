7a52916df7e360a0ce8d678bc9696ce722e29a19
ronald-it <ronald.cirgofer@hotmail.com>
2025-01-07 02:44:33 +0700

    Changed text sizes on all pages where the label text and related input where on the same line as well as placing them in columns in stead of rows. Application should now be fully responsive for iPhone as well.

--- resources/views/member-type/edit.blade.php
+++ resources/views/member-type/edit.blade.php
@@ -3,15 +3,15 @@
     Edit member type
   </x-slot>
   <div class="p-4 sm:p-8 flex flex-col items-center sm:justify-center grow">
-    <form method="POST" action="/member-type/{{$memberType->id}}" class="flex flex-col gap-y-4 sm:gap-y-6 text-xs sm:text-sm lg:text-lg w-full max-w-md">
+    <form method="POST" action="/member-type/{{$memberType->id}}" class="flex flex-col gap-y-4 sm:gap-y-6 lg:text-lg w-full max-w-md">
       @csrf
       @method('PUT')
-      <span class="uppercase font-medium text-sm sm:text-base lg:text-3xl text-center">edit member type</span>
-      <div class="flex items-center gap-x-2">
+      <span class="uppercase font-medium lg:text-3xl text-center">edit member type</span>
+      <div class="flex flex-col gap-x-2 gap-y-1">
         <label class="font-medium capitalize w-24 lg:w-28" for="id">id</label>
         <input type="number" class="border border-theme-purple rounded px-2 h-10 grow" id="id" name="id" value="{{$memberType->id}}" readonly required />
       </div>
-      <div class="flex items-center gap-x-2">
+      <div class="flex flex-col gap-x-2 gap-y-1">
         <label class="font-medium capitalize w-24 lg:w-28" for="description">description</label>
         <input type="text" class="border border-theme-purple rounded px-2 h-10 grow" id="description" name="description" value="{{$memberType->description}}" required />
       </div>
